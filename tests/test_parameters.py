09a7d9aea71e8d4881dc882242085499e775034d
Idan Gazit <idan@gazit.me>
2012-03-12 16:59:03 -0700

    Update parameter tests
    * Use a list of 2-tuples for param inputs for consistency
    * Add test for authorization headers with garbage extra data

--- tests/test_parameters.py
+++ tests/test_parameters.py
@@ -6,6 +6,21 @@ from oauthlib.parameters import *
 
 
 class ParameterTests(TestCase):
+    auth_only_params = [
+        ('oauth_consumer_key', "9djdj82h48djs9d2"),
+        ('oauth_token', "kkk9d7dh3k39sjv7"),
+        ('oauth_signature_method', "HMAC-SHA1"),
+        ('oauth_timestamp', "137131201"),
+        ('oauth_nonce', "7d8f3e4a"),
+        ('oauth_signature', "bYT5CMsGcbgUdFHObYMEfcx6bsw%3D")
+    ]
+    auth_and_data = list(auth_only_params)
+    auth_and_data.append(('data_param_foo', 'foo'))
+    auth_and_data.append(('data_param_1', 1))
+    realm = 'testrealm'
+    norealm_authorization_header = 'OAuth oauth_consumer_key=9djdj82h48djs9d2,oauth_token=kkk9d7dh3k39sjv7,oauth_signature_method=HMAC-SHA1,oauth_timestamp=137131201,oauth_nonce=7d8f3e4a,oauth_signature=bYT5CMsGcbgUdFHObYMEfcx6bsw%3D'
+    withrealm_authorization_header = 'OAuth realm=testrealm,oauth_consumer_key=9djdj82h48djs9d2,oauth_token=kkk9d7dh3k39sjv7,oauth_signature_method=HMAC-SHA1,oauth_timestamp=137131201,oauth_nonce=7d8f3e4a,oauth_signature=bYT5CMsGcbgUdFHObYMEfcx6bsw%3D'
+
     def test_order_oauth_parameters(self):
         unordered = {
             'oauth_foo': 'foo',
@@ -20,16 +35,17 @@ class ParameterTests(TestCase):
         self.assertEqual(order_oauth_parameters(unordered), expected)
 
     def test_prepare_authorization_header(self):
-        realm = 'testrealm'
-        auth_header_params = {
-            'oauth_consumer_key': "9djdj82h48djs9d2",
-            'oauth_token': "kkk9d7dh3k39sjv7",
-            'oauth_signature_method': "HMAC-SHA1",
-            'oauth_timestamp': "137131201",
-            'oauth_nonce': "7d8f3e4a",
-            'oauth_signature': "bYT5CMsGcbgUdFHObYMEfcx6bsw%3D"
-        }
-        norealm = prepare_authorization_header(auth_header_params)
-        withrealm = prepare_authorization_header(auth_header_params, realm=realm)
-        self.assertEqual(norealm, 'OAuth oauth_nonce=7d8f3e4a,oauth_timestamp=137131201,oauth_consumer_key=9djdj82h48djs9d2,oauth_signature_method=HMAC-SHA1,oauth_token=kkk9d7dh3k39sjv7,oauth_signature=bYT5CMsGcbgUdFHObYMEfcx6bsw%3D')
-        self.assertEqual(withrealm, 'OAuth realm=testrealm,oauth_nonce=7d8f3e4a,oauth_timestamp=137131201,oauth_consumer_key=9djdj82h48djs9d2,oauth_signature_method=HMAC-SHA1,oauth_token=kkk9d7dh3k39sjv7,oauth_signature=bYT5CMsGcbgUdFHObYMEfcx6bsw%3D')
+        self.assertEqual(
+            prepare_authorization_header(self.auth_only_params),
+            self.norealm_authorization_header)
+        self.assertEqual(
+            prepare_authorization_header(self.auth_only_params, realm=self.realm),
+            self.withrealm_authorization_header)
+
+    def test_prepare_authorization_header_ignore_data(self):
+        self.assertEqual(
+            prepare_authorization_header(self.auth_and_data),
+            self.norealm_authorization_header)
+        self.assertEqual(
+            prepare_authorization_header(self.auth_and_data, realm=self.realm),
+            self.withrealm_authorization_header)
