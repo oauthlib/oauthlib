4335dedcd393318421b51f523308cd162fd7595a
Idan Gazit <idan@gazit.me>
2012-03-12 11:46:59 -0700

    Minor cleanup of docstrings

--- oauthlib/utils.py
+++ oauthlib/utils.py
@@ -14,8 +14,7 @@ from random import getrandbits
 
 
 def filter_oauth_params(params):
-    """Removes all non oauth parameters from a dict or a list of params.
-    """
+    """Removes all non oauth parameters from a dict or a list of params."""
     is_oauth = lambda kv: kv[0].startswith("oauth_")
     if isinstance(params, dict):
         return filter(is_oauth, params.items())
@@ -58,9 +57,6 @@ def escape(s):
     """Escape a string in an OAuth-compatible fashion.
 
     Per `section 3.6`_ of the spec.
-
-    :param s: The string to be escaped.
-    :return: An url encoded string.
 
     .. _`section 3.6`: http://tools.ietf.org/html/rfc5849#section-3.6
 
