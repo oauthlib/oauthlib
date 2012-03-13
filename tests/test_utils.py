1c43019f3f4efb88e4f67a54aa5e00dd1a3e63f0
Daniel Greenfeld <pydanny@gmail.com>
2012-03-13 13:02:53 -0700

    added test for filtered oauth params

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,8 +7,23 @@ from oauthlib.utils import *
 
 class UtilsTests(TestCase):
 
+    sample_params_list = [
+            ("notoauth", "shouldnotbehere"),
+            ("oauth_consumer_key", "9djdj82h48djs9d2"),
+            ("oauth_token", "kkk9d7dh3k39sjv7"),
+            ("notoautheither", "shouldnotbehere")
+        ]
+
+    sample_params_dict = {
+            "notoauth":"shouldnotbehere",
+            "oauth_consumer_key":"9djdj82h48djs9d2",
+            "oauth_token":"kkk9d7dh3k39sjv7",
+            "notoautheither":"shouldnotbehere"
+        }
+
     def test_filter_params(self):
 
+        # The following is an isolated test functon used to test the filter_params decorator.
         @filter_params
         def special_test_function(params, realm=None):
             """ I am a special test function """
@@ -17,18 +32,39 @@ class UtilsTests(TestCase):
         # check that the docstring got through
         self.assertEqual(special_test_function.__doc__, " I am a special test function ")
 
-        sample_params = [
-                ("notoauth", "shouldnotbehere"),
-                ("oauth_consumer_key", "9djdj82h48djs9d2"),
-                ("oauth_token", "kkk9d7dh3k39sjv7"),
-                ("notoautheither", "shouldnotbehere")
-            ]
-
-        # Check that the filtering works as per design.
+        # Check that the decorator filtering works as per design.
         #   Any param that does not start with 'oauth'
         #   should not be present in the filtered params
-        filtered_params = special_test_function(sample_params)
+        filtered_params = special_test_function(self.sample_params_list)
         self.assertFalse("notoauth" in filtered_params)
         self.assertTrue("oauth_consumer_key" in filtered_params)
         self.assertTrue("oauth_token" in filtered_params)
         self.assertFalse("notoautheither" in filtered_params)
+
+    def test_filter_oauth_params(self):
+
+        # try with list
+        # try with list
+        # try with list
+        self.assertEqual(len(self.sample_params_list), 4)
+
+        #   Any param that does not start with 'oauth'
+        #   should not be present in the filtered params
+        filtered_params = filter_oauth_params(self.sample_params_list)
+        self.assertEqual(len(filtered_params), 2)
+        
+        self.assertTrue(filtered_params[0][0].startswith('oauth'))
+        self.assertTrue(filtered_params[1][0].startswith('oauth'))
+
+        # try with dict
+        # try with dict
+        # try with dict
+        self.assertEqual(len(self.sample_params_dict), 4)
+
+        #   Any param that does not start with 'oauth'
+        #   should not be present in the filtered params
+        filtered_params = filter_oauth_params(self.sample_params_dict)
+        self.assertEqual(len(filtered_params), 2)
+        
+        self.assertTrue(filtered_params[0][0].startswith('oauth'))
+        self.assertTrue(filtered_params[1][0].startswith('oauth'))        
