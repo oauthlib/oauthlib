1f4697bd02cd37eed49c425f776c122afd350e03
Ib Lundgren <ib.lundgren@gmail.com>
2012-03-24 18:27:23 +0100

    signature -> request_signature

--- oauthlib/oauth.py
+++ oauthlib/oauth.py
@@ -178,7 +178,7 @@ class OAuth1aServer(object):
         timestamp = params.get(u'oauth_timestamp')
         callback_uri = params.get(u'oauth_callback')
         verifier = params.get(u'oauth_verifier')
-        if not all((signature, client_key, nonce, timestamp)):
+        if not all((request_signature, client_key, nonce, timestamp)):
             return False
 
         # if version is supplied, it must be "1.0"
