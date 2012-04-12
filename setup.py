cd576c7191eb81945e417c73e2d459ba76c1c5f8
Idan Gazit <idan@gazit.me>
2012-04-12 23:07:07 +0300

    PEP8 cleanup

--- setup.py
+++ setup.py
@@ -16,17 +16,16 @@ def fread(fn):
 tests_require = ['nose', 'unittest2', 'pycrypto']
 
 setup(
-    name = 'oauthlib',
-    version = '0.0.1',
-    description = 'A generic, spec-compliant, thorough implementation of the OAuth request-signing logic',
-    long_description = fread('README.rst'),
-    author = 'Idan Gazit',
-    author_email = 'idan@gazit.me',
-    url = 'https://github.com/idangazit/oauthlib',
-    license = fread('LICENSE'),
-    packages = find_packages(exclude=('tests', 'docs')),
-    test_suite = 'nose.collector',
+    name='oauthlib',
+    version='0.0.1',
+    description='A generic, spec-compliant, thorough implementation of the OAuth request-signing logic',
+    long_description=fread('README.rst'),
+    author='Idan Gazit',
+    author_email='idan@gazit.me',
+    url='https://github.com/idangazit/oauthlib',
+    license=fread('LICENSE'),
+    packages=find_packages(exclude=('tests', 'docs')),
+    test_suite='nose.collector',
     tests_require=tests_require,
     extras_require={'test': tests_require},
 )
-
