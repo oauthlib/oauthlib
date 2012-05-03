38bde2d15e4e19afc5c3489bd72cf4dc818135d5
Idan Gazit <idan@gazit.me>
2012-05-03 08:31:33 +0300

    v0.1.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ requires = ['pycrypto']
 
 setup(
     name='oauthlib',
-    version='0.1.0',
+    version='0.1.1',
     description='A generic, spec-compliant, thorough implementation of the OAuth request-signing logic',
     long_description=fread('README.rst'),
     author='Idan Gazit',
