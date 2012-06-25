e0087a0297c152829a7b651e1174382349bb9866
Ib Lundgren <ib.lundgren@gmail.com>
2012-06-25 14:24:24 +0200

    Safe string equals

--- oauthlib/common.py
+++ oauthlib/common.py
@@ -180,6 +180,22 @@ def add_params_to_uri(uri, params):
     query = add_params_to_qs(query, params)
     return urlparse.urlunparse((sch, net, path, par, query, fra))
 
+def safe_string_equals(a, b):
+    """ Near-constant time string comparison. 
+
+    Used in order to avoid timing attacks on sensitive information such
+    as secret keys during request verification (`rootLabs`_).
+
+    .. _`rootLabs`: http://rdist.root.org/2010/01/07/timing-independent-array-comparison/
+    
+    """
+    if len(a) != len(b):
+        return False
+
+    result = 0
+    for x, y in zip(a, b):
+        result |= ord(x) ^ ord(y)
+    return result == 0
 
 class Request(object):
     """A malleable representation of a signable HTTP request.
