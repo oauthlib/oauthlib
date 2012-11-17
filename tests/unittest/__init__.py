b6a8701001d79454d4183c05eb4cbbcf4130d072
Ib Lundgren <ib.lundgren@gmail.com>
2012-11-17 12:42:43 +0100

    Form encoded fragment verification

--- tests/unittest/__init__.py
+++ tests/unittest/__init__.py
@@ -25,16 +25,21 @@ if sys.version_info[0] == 3:
 
 
 # URL comparison where query param order is insignifcant
-def url_equals(self, a, b):
+def url_equals(self, a, b, parse_fragment=False):
     parsed_a = urlparse.urlparse(a)
     parsed_b = urlparse.urlparse(b)
     query_a = urlparse.parse_qsl(a)
     query_b = urlparse.parse_qsl(b)
+    if parse_fragment:
+        fragment_a = urlparse.parse_qsl(parsed_a.fragment)
+        fragment_b = urlparse.parse_qsl(parsed_b.fragment)
+        self.assertItemsEqual(fragment_a, fragment_b)
+    else:
+        self.assertEqual(parsed_a.fragment, parsed_b.fragment)
     self.assertEqual(parsed_a.scheme, parsed_b.scheme)
     self.assertEqual(parsed_a.netloc, parsed_b.netloc)
     self.assertEqual(parsed_a.path, parsed_b.path)
     self.assertEqual(parsed_a.params, parsed_b.params)
-    self.assertEqual(parsed_a.fragment, parsed_b.fragment)
     self.assertEqual(parsed_a.username, parsed_b.username)
     self.assertEqual(parsed_a.password, parsed_b.password)
     self.assertEqual(parsed_a.hostname, parsed_b.hostname)
