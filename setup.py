0e9f5e504fad3ecfcdbcba301e72160844fcc18e
Ib Lundgren <ib.lundgren@gmail.com>
2013-02-12 00:34:27 +0100

    0.3.7

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ requires = []
 
 setup(
     name='oauthlib',
-    version='0.3.6',
+    version='0.3.7',
     description='A generic, spec-compliant, thorough implementation of the OAuth request-signing logic',
     long_description=fread('README.rst'),
     author='Idan Gazit',
