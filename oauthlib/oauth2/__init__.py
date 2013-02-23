b69c65271b29fccc39063b638fb929d2a50ace02
Ib Lundgren <ib.lundgren@gmail.com>
2013-02-23 16:36:01 +0100

    Convenince import, new names preview

--- oauthlib/oauth2/__init__.py
+++ oauthlib/oauth2/__init__.py
@@ -9,8 +9,12 @@ This module is a wrapper for the most recent implementation of OAuth 2.0 Client
 and Server classes.
 """
 
-from .draft25 import Client, Server
+from .draft25 import Client, Server, WebApplicationClient
+from .draft25 import UserAgentClient as MobileApplicationClient
+from .draft25 import PasswordCredentialsClient as LegacyApplicationClient
+from .draft25 import ClientCredentialsClient as BackendApplicationClient
 from .draft25 import AuthorizationEndpoint, TokenEndpoint, ResourceEndpoint
 from .draft25 import WebApplicationServer, MobileApplicationServer
 from .draft25 import LegacyApplicationServer, BackendApplicationServer
 from .draft25.grant_types import RequestValidator
+from .draft25.errors import *
