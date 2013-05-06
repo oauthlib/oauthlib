ea46de4a09b07ba9d2a53b66bcfea614c821edde
Stéphane Raimbault <stephane.raimbault@gmail.com>
2013-05-06 14:35:16 +0200

    Fix indentation problem in list of steps

--- oauthlib/oauth2/draft25/grant_types.py
+++ oauthlib/oauth2/draft25/grant_types.py
@@ -459,34 +459,34 @@ class AuthorizationCodeGrant(GrantTypeBase):
     The flow illustrated in Figure 3 includes the following steps:
 
     (A)  The client initiates the flow by directing the resource owner's
-            user-agent to the authorization endpoint.  The client includes
-            its client identifier, requested scope, local state, and a
-            redirection URI to which the authorization server will send the
-            user-agent back once access is granted (or denied).
+         user-agent to the authorization endpoint.  The client includes
+         its client identifier, requested scope, local state, and a
+         redirection URI to which the authorization server will send the
+         user-agent back once access is granted (or denied).
 
     (B)  The authorization server authenticates the resource owner (via
-            the user-agent) and establishes whether the resource owner
-            grants or denies the client's access request.
+         the user-agent) and establishes whether the resource owner
+         grants or denies the client's access request.
 
     (C)  Assuming the resource owner grants access, the authorization
-            server redirects the user-agent back to the client using the
-            redirection URI provided earlier (in the request or during
-            client registration).  The redirection URI includes an
-            authorization code and any local state provided by the client
-            earlier.
+         server redirects the user-agent back to the client using the
+         redirection URI provided earlier (in the request or during
+         client registration).  The redirection URI includes an
+         authorization code and any local state provided by the client
+         earlier.
 
     (D)  The client requests an access token from the authorization
-            server's token endpoint by including the authorization code
-            received in the previous step.  When making the request, the
-            client authenticates with the authorization server.  The client
-            includes the redirection URI used to obtain the authorization
-            code for verification.
+         server's token endpoint by including the authorization code
+         received in the previous step.  When making the request, the
+         client authenticates with the authorization server.  The client
+         includes the redirection URI used to obtain the authorization
+         code for verification.
 
     (E)  The authorization server authenticates the client, validates the
-            authorization code, and ensures that the redirection URI
-            received matches the URI used to redirect the client in
-            step (C).  If valid, the authorization server responds back with
-            an access token and, optionally, a refresh token.
+         authorization code, and ensures that the redirection URI
+         received matches the URI used to redirect the client in
+         step (C).  If valid, the authorization server responds back with
+         an access token and, optionally, a refresh token.
 
     .. _`Authorization Code Grant`: http://tools.ietf.org/html/rfc6749#section-4.1
     """
