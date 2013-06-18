1571f0bbb3a22bdc70172246ac0a54793b538464
Ib Lundgren <ib.lundgren@gmail.com>
2013-06-18 21:31:34 +0100

    Convenience imports for the new OAuth 1 provider.

--- oauthlib/oauth1/__init__.py
+++ oauthlib/oauth1/__init__.py
@@ -9,7 +9,11 @@ This module is a wrapper for the most recent implementation of OAuth 1.0 Client
 and Server classes.
 """
 
-from .rfc5849 import Client, Server
+from .rfc5849 import Client
 from .rfc5849 import SIGNATURE_HMAC, SIGNATURE_RSA, SIGNATURE_PLAINTEXT
 from .rfc5849 import SIGNATURE_TYPE_AUTH_HEADER, SIGNATURE_TYPE_QUERY
 from .rfc5849 import SIGNATURE_TYPE_BODY
+from .rfc5849.request_validator import RequestValidator
+from .rfc5849.endpoints import RequestTokenEndpoint, AuthorizationEndpoint
+from .rfc5849.endpoints import AccessTokenEndpoint, ResourceEndpoint
+from .rfc5849.endpoints import WebApplicationServer
