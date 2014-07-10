62604da4e5d728bf2ccc4e9e470f70a3aa180a88
mdxs <van.wijgerden@mdxs.net>
2014-07-10 16:44:51 +0200

    Show support for Python 3.4

--- setup.py
+++ setup.py
@@ -57,6 +57,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: Implementation',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
