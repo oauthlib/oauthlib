155cbdef3226462a6bc434e180e736b6918b947e
David Baumgold <david@davidbaumgold.com>
2014-09-18 10:57:46 -0400

    Use warnings.warn() to warn for scope change
    This does not raise an exception and disrupt the call stack

--- oauthlib/oauth2/rfc6749/parameters.py
+++ oauthlib/oauth2/rfc6749/parameters.py
@@ -11,6 +11,7 @@ from __future__ import absolute_import, unicode_literals
 
 import json
 import time
+import warnings
 try:
     import urlparse
 except ImportError:
@@ -322,9 +323,7 @@ def validate_token_parameters(params, scope=None):
     new_scope = params.get('scope', None)
     scope = scope_to_list(scope)
     if scope and new_scope and set(scope) != set(new_scope):
-        w = Warning("Scope has changed from {old} to {new}.".format(
+        msg = "Scope has changed from {old} to {new}.".format(
             old=scope, new=new_scope,
-        ))
-        w.old_scope = scope
-        w.new_scope = new_scope
-        raise w
+        )
+        warnings.warn(msg)
