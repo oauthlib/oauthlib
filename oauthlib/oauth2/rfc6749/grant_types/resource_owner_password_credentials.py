ae21c1f92aed9f6d0e589cafe3dcdc86fe692d6b
David Muller <dmuller@guidebook.com>
2014-09-29 13:58:39 -0700

    create/don't create refresh_token based on self.refresh_token

--- oauthlib/oauth2/rfc6749/grant_types/resource_owner_password_credentials.py
+++ oauthlib/oauth2/rfc6749/grant_types/resource_owner_password_credentials.py
@@ -106,7 +106,7 @@ class ResourceOwnerPasswordCredentialsGrant(GrantTypeBase):
             log.debug('Client error in token request, %s.', e)
             return headers, e.json, e.status_code
 
-        token = token_handler.create_token(request, refresh_token=True)
+        token = token_handler.create_token(request, self.refresh_token)
         log.debug('Issuing token %r to client id %r (%r) and username %s.',
                   token, request.client_id, request.client, request.username)
         return headers, json.dumps(token), 200
