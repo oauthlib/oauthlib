90b6ed819672d195f1f5358e4a8dff914d3f28fc
Pieter Ennes <pieter@ennes.nl>
2015-04-23 13:01:29 -0700

    Add more errors.

--- oauthlib/oauth2/rfc6749/errors.py
+++ oauthlib/oauth2/rfc6749/errors.py
@@ -143,12 +143,40 @@ class InvalidClientIdError(FatalClientError):
 
 
 class InvalidRequestError(OAuth2Error):
-
-    """The request is missing a required parameter, includes an invalid
-    parameter value, includes a parameter more than once, or is
-    otherwise malformed.
+    """
+    The request is missing a required parameter, includes an
+    unsupported parameter or parameter value, repeats the same
+    parameter, uses more than one method for including an access
+    token, or is otherwise malformed.  The resource server SHOULD
+    respond with the HTTP 400 (Bad Request) status code.
     """
     error = 'invalid_request'
+    status_code = 400
+
+
+class InvalidTokenError(OAuth2Error):
+
+    """
+    The access token provided is expired, revoked, malformed, or
+    invalid for other reasons.  The resource SHOULD respond with
+    the HTTP 401 (Unauthorized) status code.  The client MAY
+    request a new access token and retry the protected resource
+    request.
+    """
+    error = 'invalid_token'
+    status_Code = 401
+
+
+class InsufficientScopeError(OAuth2Error):
+    """
+    The request requires higher privileges than provided by the
+    access token.  The resource server SHOULD respond with the HTTP
+    403 (Forbidden) status code and MAY include the "scope"
+    attribute with the scope necessary to access the protected
+    resource.
+    """
+    error = 'insufficient_scope'
+    status_Code = 401
 
 
 class AccessDeniedError(OAuth2Error):
