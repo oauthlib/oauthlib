3e314218e1f691f91eecf7dfadb82d47fad8c185
Niels <niels@okkernoot.net>
2015-11-03 12:58:19 +0100

    Fixed OAuth2Token() breaking when scope is passed in params but has value None.

--- oauthlib/oauth2/rfc6749/tokens.py
+++ oauthlib/oauth2/rfc6749/tokens.py
@@ -28,7 +28,7 @@ class OAuth2Token(dict):
     def __init__(self, params, old_scope=None):
         super(OAuth2Token, self).__init__(params)
         self._new_scope = None
-        if 'scope' in params:
+        if 'scope' in params and params['scope']:
             self._new_scope = set(utils.scope_to_list(params['scope']))
         if old_scope is not None:
             self._old_scope = set(utils.scope_to_list(old_scope))
