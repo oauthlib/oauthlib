06c0bd8e2deb7c9c2e4c5d7874bc94c90e334d4d
Joel Stevenson <jstevenson@bepress.com>
2016-08-26 16:39:52 -0700

    Test for membership should be "not in", here too.

--- oauthlib/oauth2/rfc6749/grant_types/openid_connect.py
+++ oauthlib/oauth2/rfc6749/grant_types/openid_connect.py
@@ -96,14 +96,14 @@ class OpenIDConnectBase(GrantTypeBase):
 
     def add_id_token(self, token, token_handler, request):
         # Treat it as normal OAuth 2 auth code request if openid is not present
-        if not 'openid' in request.scopes:
+        if 'openid' not in request.scopes:
             return token
 
         # Only add an id token on auth/token step if asked for.
-        if request.response_type and not 'id_token' in request.response_type:
+        if request.response_type and 'id_token' not in request.response_type:
             return token
 
-        if not 'state' in token:
+        if 'state' not in token:
             token['state'] = request.state
 
         if request.max_age:
