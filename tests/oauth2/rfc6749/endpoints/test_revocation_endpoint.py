4297a7e3db43b5bf293537964e0f51332dcb181f
Andy Kipp <andy@rstudio.com>
2016-12-06 21:30:59 -0800

    Python 3 fixes

--- tests/oauth2/rfc6749/endpoints/test_revocation_endpoint.py
+++ tests/oauth2/rfc6749/endpoints/test_revocation_endpoint.py
@@ -61,7 +61,7 @@ class RevocationEndpointTest(TestCase):
             h, b, s = self.endpoint.create_revocation_response(self.uri,
                     headers=self.headers, body=body)
             self.assertEqual(h, {})
-            self.assertEqual(b, None)
+            self.assertEqual(b, '')
             self.assertEqual(s, 200)
 
     def test_revoke_token_public_client_authentication_failed(self):
