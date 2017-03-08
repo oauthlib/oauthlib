2b6cdfe4cdc62f521d401bf225fcc77739149174
Tiziano Perrucci <tiziano.perrucci@gmail.com>
2017-03-08 10:53:19 +0100

    Changed all implicit imports * with explicit classes imports

--- oauthlib/oauth1/__init__.py
+++ oauthlib/oauth1/__init__.py
@@ -16,4 +16,4 @@ from .rfc5849.request_validator import RequestValidator
 from .rfc5849.endpoints import RequestTokenEndpoint, AuthorizationEndpoint
 from .rfc5849.endpoints import AccessTokenEndpoint, ResourceEndpoint
 from .rfc5849.endpoints import SignatureOnlyEndpoint, WebApplicationServer
-from .rfc5849.errors import *
+from .rfc5849.errors import InsecureTransportError, InvalidClientError, InvalidRequestError, InvalidSignatureMethodError, OAuth1Error
