cfb82feb03fcd60b3b66ac09bf1b478cd5f11b7d
Viktor Haag <ViktorHaag@users.noreply.github.com>
2017-11-14 07:44:44 -0800

    Add support for HMAC-SHA256 (builds on PR#388) (#498)
    * Add support for HMAC-SHA256
    
    * Add explicit declaration of HMAC-SHA1 and point HMAC at it
    
    To avoid confusion, HMAC constant name should explicitly state which SHA variant is used, but for backwards compatibility, SIGNATURE_HMAC is still needed
    
    * add support for HMAC-SHA256 including tests and comments
    
    * constructor tests verify client built with correct signer method

--- oauthlib/oauth1/__init__.py
+++ oauthlib/oauth1/__init__.py
@@ -9,7 +9,7 @@ and Server classes.
 from __future__ import absolute_import, unicode_literals
 
 from .rfc5849 import Client
-from .rfc5849 import SIGNATURE_HMAC, SIGNATURE_RSA, SIGNATURE_PLAINTEXT
+from .rfc5849 import SIGNATURE_HMAC, SIGNATURE_HMAC_SHA1, SIGNATURE_HMAC_SHA256, SIGNATURE_RSA, SIGNATURE_PLAINTEXT
 from .rfc5849 import SIGNATURE_TYPE_AUTH_HEADER, SIGNATURE_TYPE_QUERY
 from .rfc5849 import SIGNATURE_TYPE_BODY
 from .rfc5849.request_validator import RequestValidator
