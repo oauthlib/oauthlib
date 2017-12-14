d165d96b95ff30aefb03c9549f8ae073fd95a28e
Peter Slump <github@yarf.nl>
2017-12-14 20:20:08 +0100

    Updated version of Mock

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def fread(fn):
 if sys.version_info[0] == 3:
     tests_require = ['nose', 'cryptography', 'pyjwt>=1.0.0', 'blinker']
 else:
-    tests_require = ['nose', 'unittest2', 'cryptography', 'mock', 'pyjwt>=1.0.0', 'blinker']
+    tests_require = ['nose', 'unittest2', 'cryptography', 'mock>=2.0', 'pyjwt>=1.0.0', 'blinker']
 rsa_require = ['cryptography']
 signedtoken_require = ['cryptography', 'pyjwt>=1.0.0']
 signals_require = ['blinker']
