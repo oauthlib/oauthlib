7284ce8e9b5359ff16a355657ce38c1b6335a0fc
Wiliam Souza <wiliamsouza83@gmail.com>
2018-03-17 16:36:04 -0300

    Fix doc string for rfc6749.request_validator

--- oauthlib/oauth2/rfc6749/request_validator.py
+++ oauthlib/oauth2/rfc6749/request_validator.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-oauthlib.oauth2.rfc6749.grant_types
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+oauthlib.oauth2.rfc6749.request_validator
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 """
 from __future__ import absolute_import, unicode_literals
 
