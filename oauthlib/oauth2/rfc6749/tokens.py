87b93b1624faa060ba5c12767d8550d4de0eb0f4
Wiliam Souza <wiliamsouza83@gmail.com>
2018-03-17 16:40:51 -0300

    Move JWTToken to oauthlib.openid.connect.core.tokens

--- oauthlib/oauth2/rfc6749/tokens.py
+++ oauthlib/oauth2/rfc6749/tokens.py
@@ -16,6 +16,8 @@ from binascii import b2a_base64
 from oauthlib import common
 from oauthlib.common import add_params_to_qs, add_params_to_uri, unicode_type
 
+from oauthlib.openid.connect.core.tokens import JWTToken as NewJWTToken
+
 from . import utils
 
 try:
@@ -317,41 +319,4 @@ class BearerToken(TokenBase):
             return 0
 
 
-class JWTToken(TokenBase):
-    __slots__ = (
-        'request_validator', 'token_generator',
-        'refresh_token_generator', 'expires_in'
-    )
-
-    def __init__(self, request_validator=None, token_generator=None,
-                 expires_in=None, refresh_token_generator=None):
-        self.request_validator = request_validator
-        self.token_generator = token_generator or random_token_generator
-        self.refresh_token_generator = (
-            refresh_token_generator or self.token_generator
-        )
-        self.expires_in = expires_in or 3600
-
-    def create_token(self, request, refresh_token=False, save_token=False):
-        """Create a JWT Token, using requestvalidator method."""
-
-        if callable(self.expires_in):
-            expires_in = self.expires_in(request)
-        else:
-            expires_in = self.expires_in
-
-        request.expires_in = expires_in
-
-        return self.request_validator.get_jwt_bearer_token(None, None, request)
-
-    def validate_request(self, request):
-        token = get_token_from_header(request)
-        return self.request_validator.validate_jwt_bearer_token(
-            token, request.scopes, request)
-
-    def estimate_type(self, request):
-        split_header = request.headers.get('Authorization', '').split()
-
-        if len(split_header) == 2 and split_header[0] == 'Bearer' and split_header[1].startswith('ey') and split_header[1].count('.') in (2, 4):
-            return 10
-        return 0
+JWTToken = NewJWTToken
