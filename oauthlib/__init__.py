59a0cb748ad66d7b3a59848bae22e188fd3eec5c
Jonathan Huot <JonathanHuot@users.noreply.github.com>
2018-10-05 11:32:39 +0200

    Bump version to prepare 3.0.0

--- oauthlib/__init__.py
+++ oauthlib/__init__.py
@@ -12,6 +12,6 @@ import logging
 from logging import NullHandler
 
 __author__ = 'The OAuthlib Community'
-__version__ = '2.1.0'
+__version__ = '3.0.0-dev'
 
 logging.getLogger('oauthlib').addHandler(NullHandler())
