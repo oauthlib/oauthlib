f4273e75250dd36c88d63dc075ae45650a5172e9
Jonathan Huot <jonathan.huot@thomsonreuters.com>
2018-12-28 19:24:11 +0100

    Bump to 2019

--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'OAuthLib'
-copyright = u'2018, The OAuthlib Community'
+copyright = u'2019, The OAuthlib Community'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
