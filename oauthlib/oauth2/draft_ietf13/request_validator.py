369f0b12c32ec3fdcaeefa266e96ed0bccebf9e4
James Campbell <james@printtapp.com>
2018-12-30 22:17:02 +0000

    Preparing for validator.

---
+++ oauthlib/oauth2/draft_ietf13/request_validator.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+oauthlib.oauth2.draft_ietf13.request_validator
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+from __future__ import absolute_import, unicode_literals
+
+import logging
+
+log = logging.getLogger(__name__)
+
+class RequestValidator(object):
+    pass
