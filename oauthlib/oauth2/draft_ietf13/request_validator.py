00400fdf7438df702ac974a3fc55c4a4eecac352
James Campbell <james@printtapp.com>
2019-01-07 17:04:01 +0000

    Documented save_device_code

--- oauthlib/oauth2/draft_ietf13/request_validator.py
+++ oauthlib/oauth2/draft_ietf13/request_validator.py
@@ -38,37 +38,29 @@ class RequestValidator(OAuth2RequestValidator):
         """
         return 5
 
-    def save_device_code(self):
-        """Persist the authorization_code.
+    def save_device_code(self, client_id, device_code, request):
+        """Persist the device_code.
 
         The code should at minimum be stored with:
             - the client_id (client_id)
             - the redirect URI used (request.redirect_uri)
             - a resource owner / user (request.user)
             - the authorized scopes (request.scopes)
-            - the client state, if given (code.get('state'))
+            - the expiry date
 
         The 'code' argument is actually a dictionary, containing at least a
-        'code' key with the actual authorization code:
+        'user_code', 'device_code' and 'expires_in' key with the actual authorization code:
 
-            {'code': 'sdf345jsdf0934f'}
-
-        It may also have a 'state' key containing a nonce for the client, if it
-        chose to send one.  That value should be saved and used in
-        'validate_code'.
-
-        It may also have a 'claims' parameter which, when present, will be a dict
-        deserialized from JSON as described at
-        http://openid.net/specs/openid-connect-core-1_0.html#ClaimsParameter
-        This value should be saved in this method and used again in 'validate_code'.
+        {
+            'device_code': 'sdf345jsdf0934f',
+            'user_code': 'ADFG-GHJK',
+            'expires_in': 54353
+        }
 
         :param client_id: Unicode client identifier.
-        :param code: A dict of the authorization code grant and, optionally, state.
+        :param device_code: A dict of the authorization code grant and, optionally, state.
         :param request: OAuthlib request.
         :type request: oauthlib.common.Request
-
-        Method is used by:
-            - Authorization Code Grant
         """
         raise NotImplementedError('Subclasses must implement this method.')
 
@@ -103,7 +95,7 @@ class RequestValidator(OAuth2RequestValidator):
         raise NotImplementedError('Subclasses must implement this method.')
 
 
-    def validate_user_code(self):
+    def validate_user_code(self, client_id, device_code, request):
         """Verify that the authorization_code is valid and assigned to the given
         client.
 
@@ -163,11 +155,11 @@ class RequestValidator(OAuth2RequestValidator):
         raise NotImplementedError('Subclasses must implement this method.')
 
 
-    def invalidate_device_code(self, device_code):
+    def invalidate_device_code(self, client_id, device_code, request):
         """Invalidate an device code after use.
 
         :param client_id: Unicode client identifier.
-        :param code: The device code grant (request.device_code).
+        :param device_code: The device code grant (request.device_code).
         :param request: OAuthlib request.
         :type request: oauthlib.common.Request
         """
