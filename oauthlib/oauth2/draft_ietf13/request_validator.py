30c687379f27aa7fd41ad0f5bf37ceba694dba71
James Campbell <james@printtapp.com>
2019-01-07 16:48:48 +0000

    Inherit the class from oauth2 so we can mix in the methods for the device token.

--- oauthlib/oauth2/draft_ietf13/request_validator.py
+++ oauthlib/oauth2/draft_ietf13/request_validator.py
@@ -5,9 +5,11 @@ oauthlib.oauth2.draft_ietf13.request_validator
 """
 from __future__ import absolute_import, unicode_literals
 
+from oauthlib.oauth2.rfc6749.request_validator import RequestValidator
+
 import logging
 
 log = logging.getLogger(__name__)
 
-class RequestValidator(object):
+class RequestValidator(RequestValidator):
     pass
