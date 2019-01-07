2e610a34b0049b6024506224ba5e54bfa761a341
James Campbell <james@printtapp.com>
2019-01-07 16:52:07 +0000

    Alias the original base class as I'm paranoid this will cause issues.

--- oauthlib/oauth2/draft_ietf13/request_validator.py
+++ oauthlib/oauth2/draft_ietf13/request_validator.py
@@ -5,35 +5,34 @@ oauthlib.oauth2.draft_ietf13.request_validator
 """
 from __future__ import absolute_import, unicode_literals
 
-from oauthlib.oauth2.rfc6749.request_validator import RequestValidator
+from oauthlib.oauth2.rfc6749.request_validator import RequestValidator as OAuth2RequestValidator
 
 import logging
 
 log = logging.getLogger(__name__)
 
-class RequestValidator(RequestValidator):
+class RequestValidator(OAuth2RequestValidator):
 
     def get_verification_url(self):
         pass
-        
+
     def get_verification_url_complete(self):
         pass
-        
+
     def get_interval(self):
         pass
-        
+
     def save_device_code(self):
         pass
-        
+
     def validate_device_code(self):
         pass
-        
+
     def validate_user_code(self):
         pass
-        
+
     def update_user_code_authorization_status(self):
         pass
-        
+
     def invalidate_device_code(self):
         pass
-        
