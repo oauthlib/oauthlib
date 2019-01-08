61867fd901606945c316b8b3249592311e61efa7
James Campbell <james@printtapp.com>
2019-01-08 12:19:30 +0000

    Placeholder headers.

---
+++ oauthlib/oauth2/draft_ietf13/endpoints/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+oauthlib.oauth2.rfc6749
+~~~~~~~~~~~~~~~~~~~~~~~
+
+This module is an implementation of various logic needed
+for consuming and providing OAuth 2.0 RFC6749.
+"""
+from __future__ import absolute_import, unicode_literals
