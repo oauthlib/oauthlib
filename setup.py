a68e019b7c0df2da2bc7db412f2d43653449aab0
Hugo van Kemenade <hugovk@users.noreply.github.com>
2021-10-11 18:45:05 +0300

    Add support for Python 3.9 and 3.10

--- setup.py
+++ setup.py
@@ -54,6 +54,8 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: Implementation',
         'Programming Language :: Python :: Implementation :: CPython',
