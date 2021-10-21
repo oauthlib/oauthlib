583bfdbb5ec835cffa2c69f952566365282cde9b
Scott Gifford <sgifford@activecampaign.com>
2021-10-21 09:40:11 -0600

    try, catch, and skip casting expires_at as int

--- oauthlib/oauth2/rfc6749/clients/base.py
+++ oauthlib/oauth2/rfc6749/clients/base.py
@@ -513,7 +513,10 @@ class Client:
             self._expires_at = time.time() + int(self.expires_in)
 
         if 'expires_at' in response and isinstance(response.get('expires_at'), int):
-            self._expires_at = int(response.get('expires_at'))
+            try:
+                self._expires_at = int(response.get('expires_at'))
+            except Exception:
+                pass
 
         if 'mac_key' in response:
             self.mac_key = response.get('mac_key')
