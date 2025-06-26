f0d14f2d0332889c68c368283bc006331c15f0c0
Harry Patcher <harry-patcher@users.noreply.github.com>
2025-06-26 10:38:37 +0000

    Fix validate_token_parameters scope validation

--- oauthlib/oauth2/rfc6749/parameters.py
+++ oauthlib/oauth2/rfc6749/parameters.py
@@ -459,18 +459,20 @@ def validate_token_parameters(params):
     # If the issued access token scope is different from the one requested by
     # the client, the authorization server MUST include the "scope" response
     # parameter to inform the client of the actual scope granted.
-    # https://tools.ietf.org/html/rfc6749#section-3.3
     if params.scope_changed:
         message = 'Scope has changed from "{old}" to "{new}".'.format(
             old=params.old_scope, new=params.scope,
         )
         scope_changed.send(message=message, old=params.old_scopes, new=params.scopes)
         if not os.environ.get('OAUTHLIB_RELAX_TOKEN_SCOPE', None):
-            w = Warning(message)
-            w.token = params
-            w.old_scope = params.old_scopes
-            w.new_scope = params.scopes
-            raise w
+            # Only raise if there are missing scopes (requested scopes not in returned scopes)
+            missing_scopes = set(params.old_scopes) - set(params.scopes)
+            if missing_scopes:
+                w = Warning(message)
+                w.token = params
+                w.old_scope = params.old_scopes
+                w.new_scope = params.scopes
+                raise w
 
 def parse_expires(params):
     """Parse `expires_in`, `expires_at` fields from params
